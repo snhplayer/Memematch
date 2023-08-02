afeaa2cf56652218d54bca1b458e977387acc944
snhplayer <141171730+snhplayer@users.noreply.github.com>
2023-08-02 15:02:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
+import random
 import pygame
 from menu import main_menu
-from game import game_interface
+from game import game_interface, situations
 
 # Initialize pygame
 pygame.init()
@@ -9,11 +11,15 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Meme Exchange")
 
+# Define the initial situation
+situation = random.choice(situations)
+
 # Run the main menu
 play_button_rect, settings_button_rect, quit_button_rect = main_menu(screen)
 
 # Game loop
 running = True
+show_menu = True  # Флаг для отображения главного меню
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,9 +28,19 @@ while running:
             if event.button == 1:
                 mouse_pos = pygame.mouse.get_pos()
                 if play_button_rect.collidepoint(mouse_pos):
-                    game_interface(screen)
+                    show_menu = False  # Скрыть главное меню
                 elif quit_button_rect.collidepoint(mouse_pos):
                     running = False
+
+    if show_menu:
+        # Отображение главного меню
+        play_button_rect, settings_button_rect, quit_button_rect = main_menu(screen)
+    else:
+        # Отображение игрового интерфейса
+        situation = game_interface(screen, situation)
+
+    # Обновление экрана
+    pygame.display.flip()
 
 # Quit the game
 pygame.quit()
