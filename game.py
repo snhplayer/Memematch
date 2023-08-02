8d844bf327b9b2427edf1877aed3901d3e6c5eb1
snhplayer <141171730+snhplayer@users.noreply.github.com>
2023-08-02 15:02:15 +0300

    Update game.py

--- game.py
+++ game.py
@@ -24,28 +24,23 @@ memes = [
     'images/meme10.jpg'
 ]
 
-# Функция для генерации случайной ситуации
-def generate_situation(used_situations):
-    available_situations = list(set(situations) - set(used_situations))
-    if not available_situations:
-        available_situations = situations
-    situation = random.choice(available_situations)
-    used_situations.append(situation)
-    return situation, used_situations
+# Случайная ситуация
+situation = random.choice(situations)
 
 # Функция для генерации случайных картинок
 def generate_memes(count):
     random_memes = random.sample(memes, count)
     return random_memes
 
+# Функция для генерации новой ситуации
+def generate_new_situation():
+    new_situation = random.choice(situations)
+    return new_situation
+
 # Функция для отображения игрового интерфейса
-def game_interface(screen):
+def game_interface(screen, situation):
     # Очистка экрана
     screen.fill((255, 255, 255))
-
-    # Генерация случайной ситуации и картинок
-    situation, used_situations = generate_situation(game_interface.used_situations)
-    memes = generate_memes(4)
 
     # Отображение текстового окна
     font = pygame.font.Font(None, 24)
@@ -53,75 +48,51 @@ def game_interface(screen):
     text_rect = text.get_rect(center=(400, 200))
     screen.blit(text, text_rect)
 
+    # Генерация случайных картинок
+    memes = generate_memes(4)
+
     # Отображение игральных карт
     card_width = 100
     card_height = 150
     card_spacing = 50
     card_x = (800 - (4 * card_width + 3 * card_spacing)) // 2
     card_y = 400
+    hovered_card = None
+    mouse_pos = pygame.mouse.get_pos()
     for i in range(4):
         card_rect = pygame.Rect(card_x + i * (card_width + card_spacing), card_y, card_width, card_height)
         meme_image = pygame.image.load(memes[i])
         meme_image = pygame.transform.scale(meme_image, (card_width, card_height))
+
+        # Проверка наведения на карту
+        if card_rect.collidepoint(mouse_pos):
+            hovered_card = memes[i]
+            card_rect.y -= 20  # Сдвиг карты вперед
+
         screen.blit(meme_image, card_rect)
+
+    # Отображение выдвинутой карты
+    if hovered_card is not None:
+        card_rect = pygame.Rect(card_x + memes.index(hovered_card) * (card_width + card_spacing), card_y - 20, card_width, card_height)
+        meme_image = pygame.image.load(hovered_card)
+        meme_image = pygame.transform.scale(meme_image, (card_width, card_height))
+        screen.blit(meme_image, card_rect)
+
+    # Handle mouse clicks
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            for i in range(4):
+                card_rect = pygame.Rect(card_x + i * (card_width + card_spacing), card_y, card_width, card_height)
+                if card_rect.collidepoint(mouse_pos):
+                    # Update situation and generate new images
+                    situation = generate_new_situation()
+                    memes = generate_memes(4)
+                    break
 
     # Обновление экрана
     pygame.display.flip()
 
-    # Игровой цикл
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                for i in range(4):
-                    card_rect = pygame.Rect(card_x + i * (card_width + card_spacing), card_y, card_width, card_height)
-                    if card_rect.collidepoint(mouse_pos):
-                        # Отображение выбранной картинки в центре экрана
-                        selected_meme = pygame.image.load(memes[i])
-                        selected_meme = pygame.transform.scale(selected_meme, (400, 600))
-                        selected_meme_rect = selected_meme.get_rect(center=(400, 500))
-                        screen.blit(selected_meme, selected_meme_rect)
-                        pygame.display.flip()
+    # Return the updated situation
+    return situation
 
-                        # Анимация перемещения картинки
-                        start_pos = pygame.math.Vector2(selected_meme_rect.center)
-                        end_pos = pygame.math.Vector2(text_rect.center)
-                        animation_frames = 60
-                        for frame in range(animation_frames):
-                            progress = frame / animation_frames
-                            current_pos = start_pos.lerp(end_pos, progress)
-                            screen.fill((255, 255, 255))
-                            screen.blit(text, text_rect)
-                            selected_meme_rect.center = current_pos
-                            screen.blit(selected_meme, selected_meme_rect)
-                            pygame.display.flip()
-
-                        # Ожидание нескольких секунд
-                        pygame.time.wait(3000)
-
-                        # Очистка экрана
-                        screen.fill((255, 255, 255))
-
-                        # Генерация новой ситуации и картинок
-                        situation, used_situations = generate_situation(used_situations)
-                        memes = generate_memes(4)
-
-                        # Отображение новой ситуации и картинок
-                        text = font.render(situation, True, (0, 0, 0))
-                        text_rect = text.get_rect(center=(400, 200))
-                        screen.blit(text, text_rect)
-                        for j in range(4):
-                            card_rect = pygame.Rect(card_x + j * (card_width + card_spacing), card_y, card_width, card_height)
-                            meme_image = pygame.image.load(memes[j])
-                            meme_image = pygame.transform.scale(meme_image, (card_width, card_height))
-                            screen.blit(meme_image, card_rect)
-                        pygame.display.flip()
-
-    # Quit the game
-    pygame.quit()
-
-# Использованные ситуации
-game_interface.used_situations = []
